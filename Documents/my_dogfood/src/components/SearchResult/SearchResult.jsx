2d7d8fdbccd45c3bd1347cdc16be5108978ab58a
андрей <andreikapaev@Air-andrej.ENS610EXT>
2023-05-12 15:45:41 +0700

    Добавлен рендеринг товаров по сети

--- Documents/my_dogfood/src/components/SearchResult/SearchResult.jsx
+++ Documents/my_dogfood/src/components/SearchResult/SearchResult.jsx
@@ -1,5 +1,5 @@
 import React from "react";
  
 export const SearchResult = ({searchResult}) => {
-    return <p className="main_result">По запросу Рога найдено {searchResult} товаров</p>
+    return <p className="main_result">По запросу найдено {searchResult} товаров</p>
 }
